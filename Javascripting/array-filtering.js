c4d05a602c8390f4c4445d752171cf618a3716e3
arpit1135 <44221726+arpit1135@users.noreply.github.com>
2019-10-12 00:09:22 +0530

    Update array-filtering.js

--- Javascripting/array-filtering.js
+++ Javascripting/array-filtering.js
@@ -1,4 +1,5 @@
-const numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//array filtering
+const numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12]
  const filtered = numbers.filter(
  	function evenNumbers (number) {
        return number % 2 === 0
