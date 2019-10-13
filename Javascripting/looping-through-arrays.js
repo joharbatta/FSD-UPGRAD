4ed03cc4c5b640a3eebddab5dc23a4fdf61b239a
charu vardhan <55374406+charuvardhan@users.noreply.github.com>
2019-10-13 15:18:14 +0530

    Update looping-through-arrays.js

--- Javascripting/looping-through-arrays.js
+++ Javascripting/looping-through-arrays.js
@@ -1,6 +1,6 @@
-let pets=['cat', 'dog', 'rat']
+let pets=['cat', 'dog', 'rat'] //array declared
 for(let i=0;i<pets.length;i++)
 {
-	pets[i]=pets[i]+'s'
+	pets[i]=pets[i]+'s'   //appending s to end of all aray elements
 }
 console.log(pets)
